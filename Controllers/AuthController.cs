03dc0f392c23692ae4e9e81268cb4877a4a06b9b
Rafael Cruz <marafa1997@gmail.com>
2025-09-13 08:40:19 -0600

    Second commit

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MySql.Data.MySqlClient;
+
+
+namespace VulnShop.Net.Controllers
+{
+    [Route("auth")]
+    public class AuthController : Controller
+    {
+        private readonly MySqlConnection _connection;
+        public AuthController(MySqlConnection connection) => _connection = connection;
+
+        [HttpGet("login")]
+        public IActionResult Login() => View();
+
+        [HttpPost("login")]
+        public IActionResult LoginPost(string userName , string password)
+        {
+            _connection.Open();
+
+            //Direct concatenation vulneratbility (SQL Injection)
+            var cmd = new MySqlCommand(
+                $"SELECT * FROM user WHERE userName = '{userName}' AND password= '{password}", _connection);
+
+            var reader = cmd.ExecuteReader();
+            if (reader.Read())
+            {
+                HttpContext.Session.SetString("user", userName);
+                return Redirect("/products");
+             }
+
+            return Unauthorized("Credenciales invalidas");
+        }
+    }
+}
---
+++ Controllers/CartController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MySql.Data.MySqlClient;
+
+namespace VulnShop.Net.Controllers
+{
+    [Route("cart")]
+    public class CartController : Controller
+    {
+        private readonly MySqlConnection _connection;
+        public CartController(MySqlConnection connection) => _connection = connection;
+
+        [HttpPost("add")]
+        public IActionResult Add(int productId, int quantity, decimal price)
+        {
+            _connection.Open();
+
+            //Price manipulation from client
+            var cmd = new MySqlCommand(
+                $"INSERT INTO cart(product_id, quantity, price_at_time) " +
+                $"VALUES({productId},{quantity},{price})", _connection);
+
+            cmd.ExecuteNonQuery();
+
+            return Redirect("/cart");
+        }
+    }
+}
